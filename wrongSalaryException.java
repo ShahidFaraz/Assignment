30671c5b087ad427b43f9e29dada870ee3d186d6
ShahidFaraz <89798029+ShahidFaraz@users.noreply.github.com>
2022-03-19 11:34:09 +0530

    Add files via upload

---
+++ wrongSalaryException.java
@@ -0,0 +1,12 @@
+package Assignmen3;
+
+	// Assignment 3 
+	//Program 3
+public class wrongSalaryException extends Exception {
+	public wrongSalaryException(String errorMessage)
+	{
+		super(errorMessage);
+		
+	}
+
+}
